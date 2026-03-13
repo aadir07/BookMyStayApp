fd7842333f5f00168765a7a8a44dd5ab364557e5
aadir07 <adityaranjan70707@gmail.com>
2026-03-13 08:52:54 +0530

    UC1: Add welcome message

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,6 +1,7 @@
 
 class BookMyShowApp {
     static void main() {
-
+        System.out.println("Welcome to the Hostel Booking Managment System");
+        System.out.println("System Intialized Successfully");
     }
 }
